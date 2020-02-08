5c4606e7d2e20c7a83140afbd8092b56db9a2267
Ashley E. Whitehouse <a.whitehouse@callcpc.com>
2020-02-07 21:13:48 -0600

    object notes

--- index.js
+++ index.js
@@ -1,51 +1,11 @@
-const name = "Ashley";
-const numOfYears = "12";
-const x = "24";
-//this is my comment
-console.log("My name is  " + name + ".");
-console.log(`My name is ${name}`);
-
-//we reserve some space in memory and 'name' it 'numOfYears'
-let numOfYears = 9;
-//'let' allows us to reassign
-//This references 'numOfYears' in memory - RIPS OUT the '9' and REPLACES it with a '10'
-numOfYears = 10;
-//JS is a dynamically typed language
-numOfYears = ten;
-
-// create a variable called "name" that references a string
-const name = "Ashley"
-    // create a variable called "favoriteSong" that references a string
-    const favoriteSong = "Sin Ti"
-    // create a variable called "wage" that references a number
-    const wage = 20
-    // create a variable called "age" that references a number
-    const age = 34
-    // create a variable called "onlyChild" that references a boolean
-    const onlyChild = true
-    // create a variable called "satisfied" that references a boolean
-    const satisfied = false
-    // create a variable called "favoriteThing" that references ANY primitive value
-    const favoriteThing = "tigers"
-    // print the data type of "favoriteThing"
-console.log(typeof favoriteThing);
-    // ! You heard a new song that became your favorite.
-    // Reassign the variable "favoriteSong"
-let favoriteSong = "Como puede vivir"
-    // ! You no longer like the type of data stored in "favoriteThing"
-    // Reassign "favoriteThing" to a different primitive data type and print its type
-    let favoriteThing = 3
-    // ! Happy Birthday!
-    // Reassign "age" to a new value
-    let age = 44
-    // ! You got a big raise at work.
-    // Reassign "wage" to a new value
-    let wage = 45
-
-    // ! Your raise changed your job satisfaction.
-    // Assign "satisfied" to true
-    let satisfied = true
-    // use "console.log()" and "+" to print a sentence that includes two variables
-    console.log("My name is  " + name + and + I am + "age" + ".")
-
-
+const me = {
+  age: 33,
+  name: "Ashley",
+  occ: "social media marketer"
+};
+console.log(me.age);
+console.log(me["age"]);
+let keyINeed = "age";
+console.log(me[keyINeed]);
+console.log(me["keyINeed"]);
+console.log(console);
